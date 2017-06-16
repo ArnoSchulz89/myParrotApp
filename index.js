61fe4e530353c33402eb5c7c6be5a982154efc88
Arno Schulz <arno.schulz89@gmail.com>
2017-06-16 18:04:24 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -5,16 +5,70 @@ const bodyParser = require('body-parser');
 
 const restService = express();
 
+const request = require('request');
+
 restService.use(bodyParser.urlencoded({
     extended: true
 }));
 
 restService.use(bodyParser.json());
 
+
+
+// Set the headers
+var headers = [
+    {"Accept" : "*/*"},
+
+    {"Accept-Encoding" : "gzip"},
+
+    {"Accept-Language" : "de-DE"},
+
+    {"apikey" : "Mo9ILZ1MFroWqdyNQ9O82xxRr4s6pUul"},
+
+    {"Host" : "hermestest-dev.apigee.net"},
+
+    {"Origin" : "http%3A//developer.hermesworld.co.uk"},
+
+    {"Referer" : "http%3A//developer.hermesworld.co.uk/tracking-api/apis/get/events"},
+
+    {"User-Agent" : ""},
+
+    {"X-Forwarded-For" : "5.148.154.254"},
+
+    {"X-Forwarded-Port" : "443"},
+
+    {"X-Forwarded-Proto" : "https"}
+
+
+];    
+
+// Configure the request
+var options = {
+    'url': 'https://hermestest-dev.apigee.net/tracking/devportal/events',
+    'method': 'GET',
+    'barcode': '0000000000000019',
+    //'apikey' : 'Mo9ILZ1MFroWqdyNQ9O82xxRr4s6pUul'
+    'headers' : headers
+}
+
+// Start the request
+function getTracking(){
+    restService.get(options, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            // Print out the response body
+            console.log(body)
+        }
+        return response.body.result.parameter.description
+    }
+};
+
+
+
+
+
 restService.post('/echo', function(req, res) {
     //do something with the echoText parameter.
-    var echo = req.body.result.parameters.echoText;
-    //var echo = str.toLowerCase();
+    var echo = req.body.result.parameters.echoText
 
     switch(echo.toLowerCase()){
         case "i love beer":
@@ -22,8 +76,11 @@ restService.post('/echo', function(req, res) {
             break;
 
         case "i am stupid":
-            var parrotText = "Yes, you are really stupid. Don't try to fool me!";
+            var parrotText = "I am not stupid. Don't try to fool me!";
             break;
+        case "hermes parcel":
+            var parrotText = getTracking();
+            
 
         default:
             var parrotText = echo + '. ' + echo;
@@ -93,7 +150,6 @@ restService.post('/echo', function(req, res) {
     });
 });
 */
-
 
 
 restService.listen((process.env.PORT || 8000), function() {
