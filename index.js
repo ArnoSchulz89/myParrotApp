47fc1023f69b336cd5ca8cfb4ac7f797fc9c5419
Arno Schulz <arno.schulz89@gmail.com>
2017-06-21 11:18:04 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -28,9 +28,13 @@ restService.post('/echo', function(req, res) {
     var req = req;
     //console.log(googleReq);
 
-    var hermesRes = tracking.getTracking(googleReq, myCb2);
+    var hermesRes = tracking.getTracking(googleReq, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var latest = JSON.parse(body)[0];
+            console.log(latest.point.description + ' is the state of your parcel');
+            var hermesRes = 'let me have a look ... your Hermes parcel is: ' + latest.point.description;
 
-    if(hermesRes != ''){
+            if(hermesRes != ''){
                 var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? hermesRes : "I didn't get it rigth, please Speak again."
                 return res.json({
                     speech: speech,
@@ -38,6 +42,9 @@ restService.post('/echo', function(req, res) {
                     source: 'gHomeHermesTrackingAPI'
                 });
             }
+        }
+         
+    }
 
 });
 
