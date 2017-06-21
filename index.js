2acbca5ea682ae4a0830df2fc733b1d9b420a82e
Arno Schulz <arno.schulz89@gmail.com>
2017-06-21 11:39:07 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const tracking = require('./lib/tracking');
+const answers = require('./lib/answers');
 const restService = express();
 
 restService.use(bodyParser.urlencoded({
@@ -25,14 +26,12 @@ function myCb2 (error, response, body) {
 
 restService.post('/echo', function(req, res) {
     var googleReq = req.body.result.parameters.echoText;
-    var req = req;
-    //console.log(googleReq);
 
     var hermesRes = tracking.getTracking(googleReq, function (error, response, body) {
         if (!error && response.statusCode == 200) {
             var latest = JSON.parse(body)[0];
             console.log(latest.point.description + ' is the state of your parcel');
-            var hermesRes = 'let me have a look ... your Hermes parcel is: ' + latest.point.description;
+            var hermesRes = answers.buildAnswer(latest);//'let me have a look ... your Hermes parcel is: ' + latest.point.description;
 
             if(hermesRes != ''){
                 var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? hermesRes : "I didn't get it rigth, please Speak again."
