7b2f1c9c786ad0f5ffc4a6464b26aebf533b2af2
Arno Schulz <arno.schulz89@gmail.com>
2017-06-21 12:37:35 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -13,17 +13,6 @@ restService.use(bodyParser.urlencoded({
 restService.use(bodyParser.json());
 
 
-function myCb2 (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            var latest = JSON.parse(body)[0];
-            console.log(latest.point.description + ' is the state of your parcel');
-            var hermesTrackingRes = 'let me have a look ... your Hermes parcel is: ' + latest.point.description;           
-        }
-        return hermesTrackingRes;
-         
-    }
-
-
 restService.post('/echo', function(req, res) {
     var googleReq = req.body.result.parameters.echoText;
 
@@ -31,7 +20,7 @@ restService.post('/echo', function(req, res) {
         if (!error && response.statusCode == 200) {
             var latest = JSON.parse(body)[0];
             console.log(latest.point.description + ' is the state of your parcel');
-            var hermesRes = answers.buildAnswer(latest);//'let me have a look ... your Hermes parcel is: ' + latest.point.description;
+            var hermesRes = answers.buildAnswerSentence(latest);//'let me have a look ... your Hermes parcel is: ' + latest.point.description;
 
             if(hermesRes != ''){
                 var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? hermesRes : "I didn't get it rigth, please Speak again."
