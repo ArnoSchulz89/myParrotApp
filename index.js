1e7cf2a0b5d7bd87a952023ef414044afee42053
Arno Schulz <arno.schulz89@gmail.com>
2017-06-21 10:13:39 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -3,8 +3,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const tracking = require('./lib/tracking');
-
-
 const restService = express();
 
 restService.use(bodyParser.urlencoded({
@@ -13,53 +11,40 @@ restService.use(bodyParser.urlencoded({
 
 restService.use(bodyParser.json());
 
-//var googleText = 'placeholder';
-
-
-/*function googleRequest(){
-    restService.post('/echo', function(req, res) {
-        //do something with the echoText parameter.
-        googleText = req.body.result.parameters.echoText;
-
-        switch(echo.toLowerCase()){
-            case "i love beer":
-                var parrotText = "I love beer to Arno, please get me some!";
-                break;
-
-            case "i am stupid":
-                var parrotText = "I am not stupid. Don't try to fool me!";
-                break;
-
-            case "where is my parcel":
-                var parrotText = "I have no clue.";
-                break;
-
-            default:
-                var parrotText = googleText + '. ' + googleText;
-        }
-
-        var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? parrotText : "Seems like some problem. Speak again."
-        return res.json({
-            speech: speech,
-            displayText: speech,
-            source: 'myParrotAPI'
-        });
-    });
-};
-*/
-
 
 function myCb(error, response, body) {
-    if (!error && response.statusCode == 200) {
+    if (!error && response.statusCode <= 200) {
         var latest = JSON.parse(body)[0];
         console.log(latest.point.description + ' is the state of your parcel');
+    }else{
+        switch (response.statusCode){
+            case 400:
+                console.log('bad request error code with an empty payload');
+                break;
+
+            case 401:
+                console.log('unauthorized user');
+                break;
+
+            case 404:
+                console.log('no parcel found for the supplied barcode. Returns an empty payload');
+                break;
+
+            case 429:
+                console.log('too many requests');
+                break;
+
+            case 500:
+                console.log('internal Server Error');
+                break;
+        }
     }
 };
 
 
 restService.post('/echo', function(req, res) {
     var googleReq = req.body.result.parameters.echoText;
-    console.log(googleReq);
+    //console.log(googleReq);
 
     tracking.getTracking(googleReq, function (error, response, body) {
         if (!error && response.statusCode == 200) {
@@ -68,77 +53,24 @@ restService.post('/echo', function(req, res) {
             var hermesRes = 'let me have a look ... your Hermes parcel is: ' + latest.point.description;
 
             if(hermesRes != ''){
-                var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? hermesRes : "Seems like some problem. Speak again."
+                var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? hermesRes : "I didn't get it rigth, please Speak again."
                 return res.json({
                     speech: speech,
                     displayText: speech,
-                    source: 'ArnosAPI'
+                    source: 'gHomeHermesTrackingAPI'
                 });
-            } 
+            }else{
+                return res.json({
+                    speech: speech,
+                    displayText: speech,
+                    sourche: 'gHomeHermesTrackingAPI'
+                });
+            }
         }
          
     });
 
 });
-
-
-
-
-
-/*restService.post('/slack-test', function(req, res) {
-
-    var slack_message = {
-        "text": "Details of JIRA board for Browse and Commerce",
-        "attachments": [{
-            "title": "JIRA Board",
-            "title_link": "http://www.google.com",
-            "color": "#36a64f",
-
-            "fields": [{
-                "title": "Epic Count",
-                "value": "50",
-                "short": "false"
-            }, {
-                "title": "Story Count",
-                "value": "40",
-                "short": "false"
-            }],
-
-            "thumb_url": "https://stiltsoft.com/blog/wp-content/uploads/2016/01/5.jira_.png"
-        }, {
-            "title": "Story status count",
-            "title_link": "http://www.google.com",
-            "color": "#f49e42",
-
-            "fields": [{
-                "title": "Not started",
-                "value": "50",
-                "short": "false"
-            }, {
-                "title": "Development",
-                "value": "40",
-                "short": "false"
-            }, {
-                "title": "Development",
-                "value": "40",
-                "short": "false"
-            }, {
-                "title": "Development",
-                "value": "40",
-                "short": "false"
-            }]
-        }]
-    }
-    return res.json({
-        speech: "speech",
-        displayText: "speech",
-        source: 'webhook-echo-sample',
-        data: {
-            "slack": slack_message
-        }
-    });
-});*/
-
 
 
 
