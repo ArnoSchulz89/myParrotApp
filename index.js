f643736c3f0cc015b2a8c4207f39266d30f97e9a
Arno Schulz <arno.schulz89@gmail.com>
2017-06-21 11:12:04 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -12,21 +12,13 @@ restService.use(bodyParser.urlencoded({
 restService.use(bodyParser.json());
 
 
-function myCb2 (error, response, body, req) {
+function myCb2 (error, response, body) {
         if (!error && response.statusCode == 200) {
             var latest = JSON.parse(body)[0];
             console.log(latest.point.description + ' is the state of your parcel');
-            var hermesRes = 'let me have a look ... your Hermes parcel is: ' + latest.point.description;
-
-            if(hermesRes != ''){
-                var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? hermesRes : "I didn't get it rigth, please Speak again."
-                return res.json({
-                    speech: speech,
-                    displayText: speech,
-                    source: 'gHomeHermesTrackingAPI'
-                });
-            }
+            var hermesTrackingRes = 'let me have a look ... your Hermes parcel is: ' + latest.point.description;           
         }
+        return hermesTrackingRes;
          
     }
 
@@ -36,7 +28,16 @@ restService.post('/echo', function(req, res) {
     var req = req;
     //console.log(googleReq);
 
-    tracking.getTracking(googleReq, myCb2(error, response, body, req));
+    var hermesRes = tracking.getTracking(googleReq, myCb2);
+
+    if(hermesRes != ''){
+                var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? hermesRes : "I didn't get it rigth, please Speak again."
+                return res.json({
+                    speech: speech,
+                    displayText: speech,
+                    source: 'gHomeHermesTrackingAPI'
+                });
+            }
 
 });
 
