b5521b3d637c9d9fef37049d63f8273c3589b6c7
Arno Schulz <arno.schulz89@gmail.com>
2017-06-21 14:46:11 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ restService.use(bodyParser.json());
 
 
 restService.post('/echo', function(req, res) {
-    var googleReq = req.body.result.parameters.echoText;
+    var googleReq = req.body.result.parameters.echoText.trim();
 
     var hermesRes = tracking.getTracking(googleReq, function (error, response, body) {
         if (!error && response.statusCode == 200) {
